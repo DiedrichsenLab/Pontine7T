5700f1c3a077e19bdbec4e37ae6fa7c19bf56c24
SW <39967319+switt4@users.noreply.github.com>
2022-12-16 14:50:47 -0500

    Update bsp_pcm_simulate_data.m

--- bsp_pcm_simulate_data.m
+++ bsp_pcm_simulate_data.m
@@ -15,19 +15,21 @@ switch(what)
 
 case 'simulate_data'
     num_subj = [1:length(subj_name)];
-    numSim = 50; %number of voxels to simulate
-    signal = 35; %integer signal level
-    noise = 12; %integer noise level
-    suffix = 'snr35-12'; %suffix for rawts_signal filename
-    vararginoptions(varargin,{'sn','numSim','signal','noise','suffix'});
+    numSim = 1; %number of voxels to simulate
+    signal = 1; %integer signal level
+    noise = 0; %integer noise level
+    suffix = 'allsignal'; %suffix for rawts_signal filename
+    vararginoptions(varargin,{'num_subj','numSim','signal','noise','suffix'});
     
     %Usage: bsp_pcm_simulate_data('simulate_data','sn',1,'numSim',50,'signal',3,'noise',1,'suffix','highSNR');
     
 
     load(fullfile(fullfile(baseDir,resDir,'/test_GLM_physio_task_instruc_model_tikhonov.mat')));
-    condition = [1 1 1 1 1 1 1 1 1 2]; %condition vector and model G parameters need to match
+%     condition = [1 1 1 1 1 1 1 1 1 2]; %condition vector and model G parameters need to match
+    condition = [1];
 
-    thetaSubj = theta(1:numRuns:end,1:2);  %only grab thetas for task and instruction, not constant
+%     thetaSubj = theta(1:numRuns:end,1:2);  %only grab thetas for task and instruction, not constant
+    thetaSubj = [10];
     design = X(1:numRuns:end,:);
     
     signalVector = signal*ones(numSim,1);
@@ -35,19 +37,24 @@ case 'simulate_data'
 
     for s = 1:length(num_subj);
         feature = reshape(X(s,:),((numTRs-numDummys)*numRuns),[]);
+        feature = sum(feature,2);
         feature = num2cell(feature,[1,2]);
         [M,Z] = pcm_buildModelFromFeatures(feature,'name','pontine');
-        M.Gd = [1 1];   %manually add Gd, Gc, and numGparams, as pcm_buildModelFromFeatures doesn't
-        M.Gc = [1 0; 0 1];
-        M.numGparams = 2;
+%         M.Gd = [1 1];   %manually add Gd, Gc, and numGparams, as pcm_buildModelFromFeatures doesn't
+%         M.Gc = [1 0; 0 1];
+%         M.numGparams = 2;
+        M.Gd = [1];
+        M.Gc = [1];
+        M.numGparams = 1;
         
         numVox = size(feature{1,1},1);
     
-        Ysim = pcm_makeDataset(M,thetaSubj(s,:)','design',condition','numVox',numVox,'numSim',numSim,'signal',signalVector,'noise',noiseVector);
+%         Ysim = pcm_makeDataset(M,thetaSubj(s,:)','design',condition','numVox',numVox,'numSim',numSim,'signal',signalVector,'noise',noiseVector);
+        Ysim = pcm_makeDataset(M,thetaSubj','design',condition','numVox',numVox,'numSim',numSim,'signal',signalVector,'noise',noiseVector);
 
         Y = cell2mat(Ysim)';
-        Y = sum(Y,2); %sum along condition dimension to produce single time series per simulated voxel
-        Y = reshape(Y,[],numSim);
+        %Y = sum(Y,2); %sum along condition dimension to produce single time series per simulated voxel
+        %Y = reshape(Y,[],numSim);
         resMS = ones(1,size(Y,2));
 
         filename = fullfile(fullfile(baseDir,regDir,'data',subj_name{s},sprintf('rawts_simulate_%s.mat',suffix)));
