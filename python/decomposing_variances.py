e28f1a9ee2f79e030826eab56da32e35c9100e84
incehusain <144962266+incehusain@users.noreply.github.com>
2024-01-16 19:01:19 -0800

    converted to tensor SxNxP

--- python/decomposing_variances.py
+++ python/decomposing_variances.py
@@ -10,16 +10,25 @@ site.addsitedir(path_to_func_fusion)
 
 from Functional_Fusion.dataset import decompose_pattern_into_group_indiv_noise
 
-#loading data
+#appending 2 files: 160 conditions, 2385 voxels each; and converting to tensor of form S x N x P
 
-cifti = nibabel.load('/Users/incehusain/decomposing_variances/beta_glm2_cerebellum_gray_S03.dscalar.nii')
+def get_data(structure='dentate'):
+    A = []
+    B = A
+    for i in range(3,5):
+        file_path = f'/Users/incehusain/decomposing_variances/beta_glm2_{structure}_S0{i}.dscalar.nii'
+        cifti = nibabel.load(file_path)
+        A.append(cifti.get_fdata())
+        to_tensor = numpy.array(B)
+    print("tensor:", B)
+    print("shape of tensor:", to_tensor.shape)
+    print("number of dimensions:", numpy.ndim(B))
+    print('number of voxels:', len(B[0][0]))
+    print('number of conditions:', len(B[0]))
+    print('subject 2, condition 1, voxel 3:', B[1][0][2])
+    print('subject 1, condition 1, voxel 1:', B[0][0][0])
 
-
-print(cifti.shape)
-
-data = cifti.get_fdata()
-
-unique_data = numpy.unique(data)
+get_data()
 
 
 
