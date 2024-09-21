109bff407a8af756e6cbf0e67b7391c4f7f771b5
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2024-09-21 17:12:47 -0400

    Make_deformation fields from ants

--- scripts/bold_normalization.py
+++ scripts/bold_normalization.py
@@ -161,20 +161,46 @@ def symmetrize_image(src,outname):
     return img
 
 def symmetrize_template(src_sub='S03'):
-    inf = f'{base_dir}/bold_normalization/templates/tpl-SyN_bold.nii'
+    inf = f'{base_dir}/bold_normalization/deformed/S03Sym_CC/wavrg.nii'
     outf = f'{base_dir}/bold_normalization/templates/tpl-SyNSym_bold.nii'
     symmetrize_image(inf,outf)
+
+
+def ants_transform_to_deformation_field(xfm,ref_img):
+    """Converts the ANTs transform to a deformation field"""
+    if isinstance(ref_img,str):
+        ref_img = ants.image_read(ref_img)
+    def_field = ants.transform_to_displacement_field(xfm, ref_img)
+    return def_field
+
+
+def make_deformation_fields(name,template):
+    """ Generates SPM-compatible deformation fields from the ANTs transforms"""
+    subj = pinfo['participant_id']
+    subj = subj[pinfo.good==1]
+    xfm_dir = f'{base_dir}/bold_normalization/transforms/{name}'
+    ref_img = ants.image_read(f'{base_dir}/bold_normalization/templates/{template}')
+
+    for src_sub in subj:
+        aff = ants.read_transform(f'{xfm_dir}/xfm_{src_sub}_0GenericAffine.mat')
+        warp = ants.read_transform(f'{xfm_dir}/xfm_{src_sub}_1Warp.nii.gz')
+        def_field = ants_transform_to_deformation_field(warp,ref_img)
+        fname=f'{xfm_dir}/y_{src_sub}.nii'
+        ants.image_write(def_field,fname)
+        pass
+
 
 if __name__ == '__main__':
     # make_initial_template_step3()
     
     # src_sub='S16'
     # make_initial_template()
-    # symmetrize_template()
+    symmetrize_template()
     # kw={'verbose':True}
     # normalize_bold_all('MNI2009c_T1bold','tpl-MNI152NLin2009cSym_res-1_T1w.nii',tot='SyNBold',kwargs=kw)
-    normalize_bold_all('S03Sym_Syn','tpl-S03Sym_bold.nii',tot='SyN')
-    normalize_bold_all('S03Sym_CC','tpl-S03Sym_bold.nii',tot='SyNCC')
-    normalize_bold_all('S03Sym_Agg','tpl-S03Sym_bold.nii',tot='SyNAggro',kwargs={'syn_metric':'CC','verbose':True})
-    # Save to nifti
+    # normalize_bold_all('S03Sym_Syn','tpl-S03Sym_bold.nii',tot='SyN')
+    # normalize_bold_all('S03Sym_CC','tpl-S03Sym_bold.nii',tot='SyNCC')
+    # normalize_bold_all('S03Sym_Agg','tpl-S03Sym_bold.nii',tot='SyNAggro',kwargs={'syn_metric':'CC','verbose':True})
+    # make_deformation_fields('S03Sym_Syn','tpl-S03Sym_bold.nii')
+    #  Save to nifti
     pass 
