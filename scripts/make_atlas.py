2426afa931ddef8cc66bb189433efc011fe57729
incehusain <144962266+incehusain@users.noreply.github.com>
2024-11-29 11:32:57 -0800

    estimate_emissions update

--- scripts/make_atlas.py
+++ scripts/make_atlas.py
@@ -73,6 +73,7 @@ def build_emission_pontine(K,P,atlas='MNISymCereb2'):
 
     em_model = em.MixVMF(K=K,P=P,X=X,part_vec=part_v)
     em_model.initialize(data)
+    return em_model
 
 def estimate_emission_models():
     """ Estimate emission models for dataset
@@ -109,61 +110,20 @@ def estimate_emission_models():
     return Vs    
 
 def estimate_new_atlas(cereb_Vs): 
+     
     # Get atlas for dentate 
 
-     dentate_atlas, _ = am.get_atlas('MNISymDentate1')    
-    
-    # Get all three datasets + info for the dentate
+     atlas, _ = am.get_atlas('MNISymDentate1')    
 
-     data, info,ds_obj = ds.get_dataset(base_dir,'MDTB',atlas='MNISymDentate1',type='CondRun',sess='all',subj=None)
-     cond_v = info['cond_num_uni']
-     part_v = info['run']
-
-     data2, info2, ds_obj2 = ds.get_dataset(base_dir,'Language',atlas='MNISymDentate1',type='CondRun',sess='ses-localizer_cond_fm',subj=None)
-     cond_v2 = info2['task']
-     part_v2 = info2['run']
-
-     #X2_b = ut.indicator(part_v2)
-     #data2_r_baseline = ds.remove_baseline(data2,X2_b)
-
-     #pontine7T
-
-     pontine_flat_data = ac.get_structure_data(structure='dentate',  data_dir='/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/RegionOfInterest_BOLDMNI/data/group')
-
-     cond_v3 = np.tile(np.arange(1,11),16)
-
-     part_v3 = np.repeat(np.arange(1,17), 10)
-
-     tensor_pontine = ac.flat2ndarray(pontine_flat_data, cond_v3, part_v3)
-
-     tensor_no_nans = np.nan_to_num(tensor_pontine)
-
-     tensor_avg_cond = tensor_no_nans.mean(axis=3, keepdims=1) #this is the mean activity pattern 
-
-     data3 = tensor_no_nans - tensor_avg_cond
-
-     data3_reshape = data3.reshape(16,160,3973)
-     
-     ar_model = ar.ArrangeIndependent(K=32, P=dentate_atlas.P, spatial_specific=True, remove_redundancy=False)
-    
-    # Make a design matrix
-     
-     X= ut.indicator(cond_v)
-     X2 = ut.indicator(cond_v2)
-     X3 = ut.indicator(cond_v3)
-     
-     em_model1 = em.MixVMF(K=32, P=dentate_atlas.P, X=X, part_vec=part_v)
-     em_model2 = em.MixVMF(K=32, P=dentate_atlas.P, X=X2, part_vec = part_v2)
-     em_model3 = em.MixVMF(K=32, P=dentate_atlas.P, X=X3, part_vec=part_v3 )
-
-     for k in range(len(cereb_Vs[0])):
-         em_model1.V[k] = cereb_Vs[0][k]
-    
-     for k in range(len(cereb_Vs[1])):
-         em_model2.V[k] = cereb_Vs[1][k]
+     ar_model = ar.ArrangeIndependent(K=32, P=atlas.P, spatial_specific=True, remove_redundancy=False)
          
-     for k in range(len(cereb_Vs[2])):
-         em_model3.V[k] = cereb_Vs[2][k]
+     em_model1 = build_emission_mdtb(ar_model.K,atlas.P,atlas='MNISymCereb2')
+     em_model2 = build_emission_language(ar_model.K,atlas.P,atlas='MNISymCereb2')
+     em_model3  = build_emission_pontine(ar_model.K,atlas.P,atlas='MNISymCereb2')
+
+     em_model1.V = cereb_Vs[0]
+     em_model2.V = cereb_Vs[1]
+     em_model3.V = cereb_Vs[2]
 
      em_model1.set_param_list(['kappa'])
      em_model2.set_param_list(['kappa'])
@@ -171,7 +131,7 @@ def estimate_new_atlas(cereb_Vs):
 
      M= fm.FullMultiModel(ar_model, [em_model1, em_model2, em_model3])
 
-     M.initialize([data, data2, data3_reshape])
+     M.initialize()
 
      M, _, _, _ = M.fit_em(iter=200, tol=0.01,
         fit_arrangement=True,fit_emission=True,first_evidence=True)
