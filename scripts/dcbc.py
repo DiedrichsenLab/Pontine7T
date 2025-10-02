1c1d84a02e365cf65cb86863f7e49f3c3c187b05
incehusain <ihusain5@uwo.ca>
2025-10-02 14:46:13 -0700

    compiled hard parcellations

---
+++ scripts/dcbc.py
@@ -0,0 +1,97 @@
+import ProbabilisticParcellation.util as ut
+import Functional_Fusion.dataset as ds
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+from scipy import stats
+import glob
+import numpy as np
+import os
+import time
+import torch as pt
+
+
+def calc_test_dcbc(
+    parcels, testdata, dist, max_dist=110, bin_width=5, trim_nan=False, verbose=True
+):
+    """DCBC: evaluate the resultant parcellation using DCBC
+    Args:
+        parcels (np.ndarray): the input parcellation:
+            either group parcellation (1-dimensional: P)
+            individual parcellation (num_subj x P )
+        dist (<AtlasVolumetric>): the class object of atlas
+        testdata (np.ndarray): the functional test dataset,
+                                shape (num_sub, N, P)
+        trim_nan (boolean): if true, the nan voxel label will be
+                            removed from DCBC calculation. Otherwise,
+                            we treat nan voxels are in the same parcel
+                            which is label 0 by default.
+    Returns:
+        dcbc_values (np.ndarray): the DCBC values of subjects
+    """
+
+    #
+    # if trim_nan:  # mask the nan voxel pairs distance to nan
+    #     dist[np.where(np.isnan(parcels))[0], :] = np.nan
+    #     dist[:, np.where(np.isnan(parcels))[0]] = np.nan
+
+    dcbc_values = []
+    for sub in range(testdata.shape[0]):
+        if verbose:
+            print(f"Subject {sub}", end=":")
+        tic = time.perf_counter()
+        if parcels.ndim == 1:
+            D = ut.compute_DCBC(
+                maxDist=max_dist,
+                binWidth=bin_width,
+                parcellation=parcels,
+                dist=dist,
+                func=testdata[sub].T,
+            )
+        else:
+            D = ut.compute_DCBC(
+                maxDist=max_dist,
+                binWidth=bin_width,
+                parcellation=parcels[sub],
+                dist=dist,
+                func=testdata[sub].T,
+            )
+        dcbc_values.append(D["DCBC"])
+        toc = time.perf_counter()
+        if verbose:
+            print(f"{toc-tic:0.4f}s")
+    return pt.stack(dcbc_values)
+
+
+if __name__ == "__main__":
+    
+    base_dir = "/Volumes/diedrichsen_data$/data/Cerebellum/Pontine7T/atlases/thalamus/indiv_parcellations/MDTB-ses2"
+
+    dcbc_values = calc_test_dcbc()
+
+    np.save(f"{base_dir}/MDTB_ses2_DCBC/dcbc.npy",dcbc_values)
+
+
+    
+
+def calc_test_dcbc(
+    parcels, testdata, dist, max_dist=110, bin_width=5, trim_nan=False, verbose=True
+):
+    
+    """DCBC: evaluate the resultant parcellation using DCBC
+    Args:
+        parcels (np.ndarray): the input parcellation:
+            either group parcellation (1-dimensional: P)
+            individual parcellation (num_subj x P )
+        dist (<AtlasVolumetric>): the class object of atlas
+        testdata (np.ndarray): the functional test dataset,
+                                shape (num_sub, N, P)
+        trim_nan (boolean): if true, the nan voxel label will be
+                            removed from DCBC calculation. Otherwise,
+                            we treat nan voxels are in the same parcel
+                            which is label 0 by default.
+    Returns:
+        dcbc_values (np.ndarray): the DCBC values of subjects
+    """
+
+
